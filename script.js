cc5edcf5e2bae452282956f1638e45bf178f7583
Saket Kesar <93246904+Saketkesar@users.noreply.github.com>
2022-05-28 13:29:48 +0530

    my linktree

---
+++ script.js
@@ -0,0 +1,9 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Programmer","Developer", "Blogger", "Designer", "Hacker","Student :)"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
